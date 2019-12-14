60246b1dc65662f8a60acaae67702fbc66a19872
tanatips <tanatip_s@hotmail.com>
2019-12-14 09:26:56 +0700

    Modify Upload file multiple file

--- src/TestUploadFile.js
+++ src/TestUploadFile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './progress.css'
 import * as Upload from './UploadFile';  
 class TestUpLoadFile extends React.Component{
     constructor(pros){
@@ -8,16 +9,37 @@ class TestUpLoadFile extends React.Component{
     handleClick (e){
         var file = document.getElementById("filename");
         var url = "http://localhost:9000/upload";
-        Upload.SendFile(file,url);
+        var resultUpload = document.getElementById("resultUpload");
+       
+        Upload.SendFile(file,url,resultUpload);
     }
     render(){
         return( 
-            <div className="App">
+            <div className="">
                 <h4>Test Upload File </h4>
                 <br/>
-                <input type="file" name ="filename" id="filename"/>
-                <input type="button" value="Upload" onClick={this.handleClick} />
-            
+                
+                <div className="dev_center">
+                    
+                <table border="0" className="App" width="100%" >
+                    <tbody>
+                        <tr><td colSpan="2"> <div id="dropbox" className="dropbox">
+                
+                            </div></td></tr>
+                    <tr>
+                        <td><input type="file" name ="filename[]" id="filename" multiple/>
+                        
+                        <input type="button" value="Upload" className="button" onClick={this.handleClick} /></td>
+                    </tr>
+                    <tr>
+                        <td colSpan="2">
+                           <div id="resultUpload"></div>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+                </div>
+                             
             </div>
         );
     }
--- src/UploadFile.js
+++ src/UploadFile.js
@@ -2,16 +2,42 @@ class UploadFile
 {
     constructor(){
     }
-    static SendFile(FILENAME,URL){
-        var file = FILENAME.files[0];
+    static SendFile(FILENAME,URL,RESULTUPLOAD){
+        var lstResult=[];
+        lstResult.push("<table width='100%' id='listUpload'>");
+        lstResult.push("<tbody>");
+        lstResult.push("<tr><th width='10%'>No</th><th width='30%'>File Name</th> <th width='60%'>Progress</th></tr>");
+        var rank = new Array(); 
+        for(var j = 0;j<FILENAME.files.length;j++)
+        {
+          var tmp = Math.floor((Math.random()*100000)+3);
+          rank.push(tmp);
+          lstResult.push("<tr><td>"+(j+1)+"</td><td>"+FILENAME.files[j].name+"</td><td><div class='myProgress' id='progress"+tmp+"'><div class='myBar' id='bar"+tmp+"'></div></div></td></tr>");
+        }
+        lstResult.push("</tbody>");
+        lstResult.push("</table>");
+        RESULTUPLOAD.innerHTML = lstResult.join("");
+        for(var i = 0;i<FILENAME.files.length;i++)
+        { 
+          this.Upload(FILENAME.files[i],URL,rank[i]);
+        }
+    }
+    static Upload(file,url,rank){
             var fd = new FormData();
             fd.append("filename", file);
-            var xhr = new XMLHttpRequest();
-            xhr.open('POST', URL, true);
-            xhr.upload.onprogress = function(e) {
+            var xhr = new Array();
+           
+            xhr[rank] = new XMLHttpRequest();
+            xhr[rank].open('POST', url, true);
+            xhr[rank].upload.onprogress = function(e) {
               if (e.lengthComputable) {
                 var percentComplete = (e.loaded / e.total) * 100;
                 console.log(percentComplete + '% uploaded');
+                var bar = document.getElementById("bar"+rank);
+                if(bar!= null){
+                  bar.style.width=percentComplete+"%";
+                  bar.innerHTML=Math.round(percentComplete)+"%";
+                }
               }
             };
             xhr.onload = function() {
@@ -21,8 +47,8 @@ class UploadFile
                 
               };
             };
-            xhr.send(fd);
-         
+            xhr[rank].send(fd);
     }
+
 } 
 module.exports = UploadFile;
